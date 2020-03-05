64c494bf48b96e411a529cce1f0c043f40226674
tzy-cret <tzy989@yeah.net>
2020-03-05 12:31:17 +0800

    helloword

---
+++ HelloWord.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+	public static void main(String[] args){
+	
+	System.out.println("Hello Word");
+	System.out.println("1 Word");
+	System.out.println("2 Word");
+	System.out.println("3 Word");
+	System.out.println("4 Word");
+	
+	}
+}
