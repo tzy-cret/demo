074bd254d22f70dc7c32bf12304996c63b4c9902
tzy-cret <tzy989@yeah.net>
2020-03-05 12:51:20 +0800

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	public static void main(String[] args){
+
+		System.out.println("Hello1 World");
+		System.out.println("Hello3 World");
+		System.out.println("Hello World");
+		System.out.println("Hello2 World");
+		
+
+	}
+}
